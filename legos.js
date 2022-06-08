ffacce8d48792aab163243c6d8371557e36596a8
bwarren3 <bwarren3@cs6261-vm05.uwg.westga.edu>
2022-06-08 08:48:26 -0400

    Added LegoPile constructor

--- legos.js
+++ legos.js
@@ -1,6 +1,18 @@
 module.exports.Brick = Brick;
+module.exports.LegoPile = LegoPile;
 
 function Brick(size, color) {
   this.size = size;
   this.color = color;
+}
+
+function LegoPile() {
+  this.dictionary = {
+    "red": new BST(),
+    "green": new BST(),
+    "blue": new BST(),
+    "yellow": new BST(),
+    "black": new BST(),
+    "white": new BST(),
+  }
 }
